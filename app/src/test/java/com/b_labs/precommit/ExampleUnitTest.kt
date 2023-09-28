cdb5595fd11dfb279fecd02ec85f5633b62d6e9c
Andrew Yaakoub <39156362+Andrew2120@users.noreply.github.com>
2023-09-29 02:32:14 +0300

    hello

--- app/src/test/java/com/b_labs/precommit/ExampleUnitTest.kt
+++ app/src/test/java/com/b_labs/precommit/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.b_labs.precommit
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
