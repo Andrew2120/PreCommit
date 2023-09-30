7cba0a8f1bd622d10833b0c85ea0876f059f583a
Andrew Yaakoub <39156362+Andrew2120@users.noreply.github.com>
2023-10-01 01:59:57 +0300

    commit11

--- app/src/main/java/com/blabs/precommit/MainActivity.kt
+++ app/src/main/java/com/blabs/precommit/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Hello !!!!!!!!!!!!!!!!!!!!!!!!dsadsad!!!sadsa!!!!!!QQQ!!")
+                    Greeting("Android Hello !!!!!!!!!!!!!!!!!!!111!!!!!dsadsad!!!sadsa!!!!!!QQQ!!")
                 }
             }
         }
